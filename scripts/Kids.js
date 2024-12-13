3a8e95f5ac24183cd6929cf2312e6fe6dee74ac8
Lindsay Joy Bowman <lindsayjoybowman2017@gmail.com>
2024-12-13 12:01:48 -0600

    last commit

--- scripts/Kids.js
+++ scripts/Kids.js
@@ -2,14 +2,32 @@ import { getChildren } from "./database.js"
 
 const children = getChildren()
 
+document.addEventListener(
+    "click",
+    (clickEvt) => {
+        const childTarget = clickEvt.target
+
+        if (childTarget.dataset.type === "child") {
+            window.alert(` ${childTarget.dataset.name} would like ${childTarget.dataset.wish} wish`)
+        }
+    }
+)
+
 export const Kids = () => {
-    let html = "<ol>"
+    let html = "<ul>"
 
     for (const child of children) {
-        html += `<li data-id="${child.id}" data-type="child" data-wish="${child.wish}">${child,name}</li>`
+        html += `<li 
+        data-id="${child.id}" 
+        data-type="child"
+        data-wish="${child.wish}"
+        id=--${child.id}
+        data-name="${child.name}"
+        >${child.name}
+        </li>`
     }
 
-    html += "</ol>"
+    html += "</ul>"
     return html
 }
 
--- scripts/Pairings.js
+++ scripts/Pairings.js
@@ -1,16 +1,18 @@
-import { getChildren } from "./database.js"
+import { getChildren, getCelebrities } from "./database.js"
 
 const kids = getChildren()
 const celebrities = getCelebrities()
 
-const findCelebrityMatch = (kidObject, celebrityArray) => {
-    let celebrity = null
+const findCelebrityMatch = (child, AllChildren) => {
+    let matchedCelebrity = null
 
-    for (const celebrity of celebrtyArray) {
-
+    for (const celebrity of AllChildren) {
+        if (celebrity.id === child.celebrityId) {
+          matchedCelebrity = celebrity  
+        }
     }
 
-    return celebritiy
+    return matchedCelebrity
 }
 
 export const Pairings = () => {
@@ -21,7 +23,7 @@ export const Pairings = () => {
         const kidsStar = findCelebrityMatch(kid, celebrities)
         html += `
             <li>
-                ${kid.name} will be making memories with ${kidsStar.name}, a ${kidsStar.Sport} star, by ${kid.wish}
+                ${kid.name} will be making memories with ${kidsStar.name}, a ${kidsStar.sport} star, by ${kid.wish}
             </li>
         `
     }
